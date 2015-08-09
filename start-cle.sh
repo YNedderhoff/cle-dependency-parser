d9976cfeda7908e4cd0f55fd376c67226dd26517
YNedderhoff <yanicknedderhoff@gmail.com>
2015-08-09 19:57:34 +0200

    Final commit before submitting the homework.

--- start-cle.sh
+++ start-cle.sh
@@ -9,18 +9,13 @@ PREDICTIONS="predictions"
 EVALUATIONS="evaluations"
 
 #train (on small set)
-python -u dependency-parser.py -train -i $CORPORA/train/wsj_train.first-5k.conll06 -m model -e 10
-#python -u dependency-parser.py -train -i $CORPORA/train/wsj_train_super_small.conll06 -m model -e 1
-#python -u dependency-parser.py -train -i $CORPORA/train/wsj_train_hyper_small.conll06 -m model -e 1
+#python -u dependency-parser.py -train -i $CORPORA/train/wsj_train.first-5k.conll06 -m model -e 10
 
 #train
-#python -u dependency-parser.py -train -i $CORPORA/train/wsj_train.conll06 -m $MODELS/model -e 2 -decrease-alpha
+python -u dependency-parser.py -train -i $CORPORA/train/wsj_train.conll06 -m $MODELS/model -e 2 -decrease-alpha
 
 #test
 python -u dependency-parser.py -test -i $CORPORA/dev/wsj_dev_without_head.conll06 -m model -o predicted.conll06
 
 #evaluate sentence based
-python -u dependency-parser.py -ev -i predicted.conll06 -g $CORPORA/dev/wsj_dev.conll06 -o evaluation_sentence.txt
-
-#evaluate arc based
-./eval07.pl -g $CORPORA/dev/wsj_dev.conll06 -s  predicted.conll06 >> evaluation.txt 2>&1
+python -u dependency-parser.py -ev -i predicted.conll06 -g $CORPORA/dev/wsj_dev.conll06 -o evaluation.txt
